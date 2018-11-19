3335461fc7684a2760995626d11e2c9dcfd4af67
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2018-11-19 14:17:57 +0100

    Added a Search Box to the Student Index page

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -29,13 +29,19 @@
 
         public IList<Student> Student { get; set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentFilter = searchString;
 
             IQueryable<Student> studentIQ = from s in _context.Student
                                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
+                                                 || s.FirstMidName.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
