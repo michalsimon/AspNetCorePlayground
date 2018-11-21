8a3d2ba3441e9d1e849df26026a3c573eb6dfa81
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2018-11-21 14:18:12 +0100

    Instructors pages scaffolded

---
+++ ContosoUniversity/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity.Models.SchoolViewModels
+{
+    using System.Collections.Generic;
+
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+
+        public IEnumerable<Course> Courses { get; set; }
+
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
