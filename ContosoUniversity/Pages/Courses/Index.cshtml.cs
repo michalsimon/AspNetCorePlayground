e9000b4bddd2f57d00ea003d00e2444e42c831ab
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2018-11-21 14:14:26 +0100

    Courses pages updated

--- ContosoUniversity/Pages/Courses/Index.cshtml.cs
+++ ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -1,29 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Models;
-
-namespace ContosoUniversity.Pages.Courses
+﻿namespace ContosoUniversity.Pages.Courses
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using ContosoUniversity.Models;
+
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+
     public class IndexModel : PageModel
     {
-        private readonly ContosoUniversity.Models.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Course> Course { get;set; }
+        public IList<Course> Course { get; set; }
 
         public async Task OnGetAsync()
         {
             Course = await _context.Courses
-                .Include(c => c.Department).ToListAsync();
+                         .Include(c => c.Department)
+                         .AsNoTracking()
+                         .ToListAsync();
         }
     }
 }
