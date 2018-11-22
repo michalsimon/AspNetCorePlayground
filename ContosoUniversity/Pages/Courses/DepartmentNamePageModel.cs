6ddf630f9f313283427646194e36e48b48f990d4
Michał Simoniuksztis <m.simoniuksztis@i4b.pl>
2018-11-22 12:11:48 +0100

    Class DepartmentNamePageModel added

---
+++ ContosoUniversity/Pages/Courses/DepartmentNamePageModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ContosoUniversity.Pages.Courses
+{
+    using System.Linq;
+
+    using ContosoUniversity.Models;
+
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DepartmentNamePageModel : PageModel
+    {
+        public SelectList DepartmentNameSL { get; set; }
+
+        public void PopulateDepartmentsDropDownList(SchoolContext _context,
+                                                    object selectedDepartment = null)
+        {
+            var departmentsQuery = from d in _context.Departments
+                                   orderby d.Name // Sort by name.
+                                   select d;
+
+            DepartmentNameSL = new SelectList(departmentsQuery.AsNoTracking(),
+                "DepartmentID", "Name", selectedDepartment);
+        }
+    }
+}
